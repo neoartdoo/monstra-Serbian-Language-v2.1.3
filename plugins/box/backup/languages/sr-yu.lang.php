9cf35dd31ff2c93070bbe393f8bf97730616a24f
Mapadesign <panic.marjan@gmail.com>
2013-01-07 20:27:43 +0100

    Monstra Serbian Language v 2.1.3
    Monstra Serbian

---
+++ plugins/box/backup/languages/sr-yu.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+    return array(
+        'backup' => array(
+    		'Backups' => 'Bekapovi',
+            'Backup date' => 'Backup datum',
+    		'Create backup' => 'Kreiraj Bekap',
+    		'Delete' => 'Obriši',
+            'storage' => 'lokacija arhive',
+            'public' => 'jvano',
+            'plugins' => 'dodaci',
+            'Size' => 'veličina',
+            'Actions' => 'Akcija',
+            'Delete backup: :backup' => 'Obriši bekap: :backup',
+            'Creating...' => 'Kreiranje...',
+        )
+	);
